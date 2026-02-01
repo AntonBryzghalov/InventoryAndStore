6b1d2cf782556ee35e635fdb6d077851fa463884
Anton Bryzghalov <absamurai@gmail.com>
2026-02-01 13:15:34 +0200

    Added ItemInfo and basic set of items.

---
+++ Assets/Scripts/ItemInfo.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemInfo", menuName = "Inventory Game/ItemInfo")]
+public class ItemInfo : ScriptableObject
+{
+    [SerializeField] private string name;
+    [SerializeField] private string description;
+    [SerializeField] private Sprite sprite;
+    [SerializeField] private int basePrice;
+
+    public string Name => name;
+    public string Description => description;
+    public Sprite Sprite => sprite;
+    public int BasePrice => basePrice;
+}

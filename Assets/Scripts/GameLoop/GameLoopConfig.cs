b392dcea4602760cc76d39632240a24de8ad5447
Anton Bryzghalov <anton@kooply.com>
2026-02-02 15:02:45 +0200

    Created game loop config

---
+++ Assets/Scripts/GameLoop/GameLoopConfig.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace InventoryGame.GameLoop
+{
+    [CreateAssetMenu(fileName = "GameRules", menuName = "Inventory Game/Game Loop Config")]
+    public class GameLoopConfig : ScriptableObject
+    {
+        [SerializeField] private int goldGivenEachCycle = 1500;
+        [SerializeField] private int basicItemsToBuy = 3;
+        [SerializeField] private int winnerGoldReward = 500;
+        [SerializeField] private int evenGoldReward = 250;
+
+        public  int GoldGivenEachCycle => goldGivenEachCycle;
+        public int BasicItemsToBuy => basicItemsToBuy;
+        public int WinnerGoldReward => winnerGoldReward;
+        public int EvenGoldReward => evenGoldReward;
+    }
+}
